7b499ca19e14d5736269255af54972748ea7b3a1
Benas Bagdanovas <benas.bagdanovas@gmail.com>
2022-05-23 23:41:24 +0300

    vue add pwa

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,9 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
-// SERVICE WORKER FOR PWA
-import { registerSW } from "virtual:pwa-register";
-
-if ("serviceWorker" in navigator) {
-  // && !/localhost/.test(window.location)) {
-  registerSW();
-}
-// END OF SERVICE WORKER FOR PWA
-
 import App from "./App.vue";
 import router from "./router";
+import "./registerServiceWorker";
 
 const app = createApp(App);
 
