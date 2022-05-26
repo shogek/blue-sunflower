5aeab30b9edf22f3ccc676826317f380f957ee26
Benas Bagdanovas <benas.bagdanovas@gmail.com>
2022-05-26 19:53:39 +0300

    512

--- vite.config.ts
+++ vite.config.ts
@@ -54,7 +54,7 @@ export default defineConfig({
           type: "image/png",
         },
         {
-          src: "./public/img/icons/pwa-512x512.png",
+          src: "pwa-512x512.png",
           sizes: "512x512",
           type: "image/png",
         },
