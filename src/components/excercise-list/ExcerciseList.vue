3f315ecdf0ff1789e51b79425a4eb3388a978846
Benas Bagdanovas <benas.bagdanovas@gmail.com>
2022-06-03 17:59:01 +0300

    Add excercise and tracker cards

---
+++ src/components/excercise-list/ExcerciseList.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import ExcerciseItem from "../excercise-item/ExcerciseItem.vue";
+</script>
+
+<template>
+<div>
+    <h2>
+        Excercises
+    </h2>
+
+    <div class="list">
+        <ExcerciseItem
+            excercise-name="Neutral grip pull-ups"
+            :sets-count="2"
+            :total-reps="12"
+            :max-reps="7"
+        />
+
+        <ExcerciseItem
+            excercise-name="Jumping jacks"
+            :sets-count="2"
+            :total-reps="450"
+            :max-reps="250"
+        />
+
+        <ExcerciseItem
+            excercise-name="Push ups"
+            :sets-count="3"
+            :total-reps="90"
+            :max-reps="35"
+        />
+    </div>
+</div>
+</template>
+
+<style scoped>
+.list {
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+}
+</style>
---
+++ src/components/tracker-item/TrackerItem.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+defineProps<{
+    /** Ex.: "Coffee" */
+    name: string
+    /** Ex.: "How many cups of coffee did you drink today?" */
+    description: string
+    /** Ex.: "Cups" (because I drank coffee from cups) */
+    measurement: string
+    /** Ex.: 2 (because I drank 2 cups of coffee today) */
+    quantity: string
+}>();
+</script>
+
+<template>
+    <div class="wrapper">
+        <h3>{{ name }}</h3>
+
+        <p class="description">{{ description }}</p>
+
+        <div class="counter">
+            <span>{{ measurement }}</span>
+            <span>{{ quantity }}</span>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.wrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    gap: 16px;
+    width: 47.8%;
+    padding: 16px;
+    border-radius: 8px;
+    border: var(--card-border);
+    box-shadow: var(--card-box-shadow);
+}
+
+.description {
+    line-height: 1;
+    font-weight: 500;
+    color: var(--color-subheading)
+}
+
+.counter {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 4px 12px;
+    border-radius: 16px;
+    background: var(--gradient-information);
+    color: var(--color-heading);
+}
+
+.counter span {
+    font-weight: 500;
+    font-size: 0.8rem;
+}
+</style>
